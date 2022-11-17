6a011f0d52c141f9a6e7307a3ac84d6f3b1e9654
LeoG0026 <79181559+LeoG0026@users.noreply.github.com>
2022-11-18 00:19:19 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Homepage from './Homepage';
+import Homepage from './Login';
+import Homepage from './Register';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Homepage />
+    <Login />
+    <Register />
   </React.StrictMode>
 );
 
